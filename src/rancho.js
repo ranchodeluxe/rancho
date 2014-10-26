f25477638fb5c8be275694e80edefcde3eb87863
Greg Corradini <gregcorradini@gmail.com>
2014-10-26 23:43:57 +0000

    make browser and requireJS compatible

--- src/rancho.js
+++ src/rancho.js
@@ -1,11 +1,19 @@
 var AbstractController = require('./abstract_controller.js');
 var object_extend = require('./base_class_utils.js').object_extend;
+var console = require("console-browserify");
 
+// CommonJS
+if ( typeof window !== "undefined" ) {
 
-if ( typeof window !== 'undefined' ) {
+    Rancho = window.Rancho || {};  
+    Rancho.AbstractController =  AbstractController;
+    Rancho.utils = { object_extend : object_extend  };
 
-    window.Rancho = window.Rancho || {};  
-    window.Rancho.AbstractController =  AbstractController;
-    window.Rancho.utils = { object_extend : object_extend  };
+// <script>
+} else if ( typeof exports === "object" && typeof module === "object" ) {
+
+    module.exports.Rancho = {};
+    module.exports.Rancho.AbstractController = AbstractController;
+    module.exports.Rancho.utils = { object_extend : object_extend  };
 
 }
