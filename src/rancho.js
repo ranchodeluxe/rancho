a4b8465d5ae31d6a20ede8fbd17ab196bb0f5fcb
Greg Corradini <gregcorradini@gmail.com>
2014-10-29 03:44:45 +0000

    expose ClassExtend as public

--- src/rancho.js
+++ src/rancho.js
@@ -1,5 +1,6 @@
 var AbstractController = require('./abstract_controller.js') ,
     object_extend = require('./base_class_utils.js').object_extend ,
+    ClassExtend = require('./base_class_utils.js').ClassExtend ,
     formatters = require( './formatters' ) ,
     console = require("console-browserify");
 
@@ -8,7 +9,8 @@ var AbstractController = require('./abstract_controller.js') ,
 if ( typeof window !== "undefined" ) {
 
     Rancho = window.Rancho || {};  
-    Rancho.AbstractController =  AbstractController;
+    Rancho.AbstractController = AbstractController;
+    Rancho.ClassExtend = ClassExtend;
     Rancho.utils = { object_extend : object_extend  };
     Rancho.formatters = formatters;
 
@@ -16,6 +18,7 @@ if ( typeof window !== "undefined" ) {
 } else if ( typeof exports === "object" && typeof module === "object" ) {
 
     module.exports.AbstractController = AbstractController;
+    module.exports.ClassExtend = ClassExtend;
     module.exports.utils = { object_extend : object_extend  };
     module.exports.formatters = formatters;
 
