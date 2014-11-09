7f40ce451956b0953ae036fe4ebe364ff72ca13f
Greg Corradini <gregcorradini@gmail.com>
2014-11-09 16:33:42 +0000

    more base for the example

--- examples/map.js
+++ examples/map.js
@@ -12,7 +12,6 @@ var MapView = Rancho.AbstractController.extend({
         on_initialize : function () {
             console.log( 'on_initialize' );
             this.bind_event_subscribers();    
-            this.render();    
         } ,
 });
 
@@ -27,9 +26,14 @@ MapView.prototype.default_options = {
     map_element : 'map' ,
     rsidebar_element : 'rsidebar' ,
     map : null , // object ref
-    rsidebar : null ,
-    crosshair_icon : null ,
+    rsidebar : null , // object_ref
+    crosshair_icon : null , // object_ref
     crosshair : null , // object ref
+    currentmouse : L.latLng(0,0) ,
+    currentproj : "3857" ,
+    projdefs : {"4326":L.CRS.EPSG4326, "3857":L.CRS.EPSG3857} ,
+    proj4defs : null ,
+
     
     // element selectors
     info_toggle_selector : '#info-toggle-button' ,
@@ -47,10 +51,6 @@ MapView.prototype.default_options = {
     center_selector : '#center' ,
     center_merc_selector : '#centermerc' , 
 
-    // templates
-    info_box_selector: '#info-box' ,
-    info_box_template:  _.template( $( '#info-box-template' ).html() ) ,
-
 };
 
 
@@ -59,54 +59,52 @@ MapView.prototype.bind_event_subscribers = function() {
     console.log( 'bind_even_subscribers' );
 
     // info toggle
-    this.$info_toggle.click(function(){
-        this.$wgs_label.fadeToggle(200);
-        this.$proj_label.fadeToggle(200);
+    this.$info_toggle.click( function(){
+        //this.$wgs_label.fadeToggle(200);
+        //this.$proj_label.fadeToggle(200);
         this.$info.delay(300).slideToggle(200);
 
-        var buttonText = this$info_toggle.text();
+        var buttonText = this.$info_toggle.text();
         if ( /hide/i.test( buttonText ) ) {
             this.$info_toggle.text('Show Panels');
         } else {
             this.$info_toggle.text('Hide Panels');
         }
-    });
+    }.bind( this ) );
 
     var activate_cb = function( e ) {
         active = $( e.target ).hasClass('active');
-        if(active){
-            //do nothing
+        if( active ){
+            // do nothing
         } else {
             this.$wgs_label.toggleClass('active');
-            this.$wgs_label.toggle();
             this.$proj_label.toggleClass('active');
-            this.$proj_label.toggle();
         }
     };
     // panel toggles
-    this.$proj_label.click( activate_cb );
-    this.$wgs_label.click( activate_cb );
+    this.$proj_label.click( activate_cb.bind( this ) );
+    this.$wgs_label.click( activate_cb.bind( this ) );
 
 
     // handle mouse moves
-    this.map.on('mousemove', function(e) {
-        currentmouse.lat = e.latlng.lat;
-        currentmouse.lng = e.latlng.lng;
+    this.map.on( 'mousemove', function(e) {
+        this.currentmouse.lat = e.latlng.lat;
+        this.currentmouse.lng = e.latlng.lng;
         this.$tilelevel.text( this.format_tile(e.latlng, this.map.getZoom()) );
         this.$mouse_position.text( this.format_point(e.latlng,'4326') );
-        this.$mouse_position_merc.text( this.format_point(e.latlng,currentproj) );
+        this.$mouse_position_merc.text( this.format_point(e.latlng, this.currentproj) );
         this.$mapbounds.text( this.format_bounds( this.map.getBounds(),'4326') );
-        this.$mapbounds_merc.text( this.format_bounds( this.map.getBounds(),currentproj) );
+        this.$mapbounds_merc.text( this.format_bounds( this.map.getBounds(), this.currentproj) );
         this.$center.text( this.format_point( this.map.getCenter(),'4326') );
-        this.$center_merc.text( this.format_point( this.map.getCenter(),currentproj) );
-    });
+        this.$center_merc.text( this.format_point( this.map.getCenter(), this.currentproj) );
+    }.bind( this ) );
 
-    this.map.on('zoomend', function(e) {
-        this.$tilelevel.text( this.format_tile(currentmouse, this.map.getZoom()) );
+    this.map.on( 'zoomend', function(e) {
+        this.$tilelevel.text( this.format_tile( this.currentmouse, this.map.getZoom() ) );
         this.$zoomlevel.text( this.map.getZoom().toString() );
         this.$mapbounds.text( this.format_bounds( this.map.getBounds(),'4326') );
-        this.$mapbounds_merc.text( this.format_bounds( this.map.getBounds(),currentproj) );
-    });
+        this.$mapbounds_merc.text( this.format_bounds( this.map.getBounds(), this.currentproj) );
+    }.bind( this ) );
 
 };
 
@@ -129,18 +127,17 @@ MapView.prototype.add_crosshair = function () {
     });
     this.crosshair = new L.marker( this.map.getCenter(), {icon: this.crosshair_icon, clickable:false} );
     this.crosshair.addTo( this.map );
-    this.map.on('move', function(e) {
+    this.map.on( 'move', function(e) {
         this.crosshair.setLatLng( this.map.getCenter() );
-    });
-
+    }.bind( this ) );
 
 };
 
 MapView.prototype.setup = function () {
     console.log( 'setup' );
 
-    this.map = L.map('map').setView([47.589841, -122.319202], 12);
-    L.tileLayer('http://{s}.tiles.mapbox.com/v3/spatialdev.map-c9z2cyef/{z}/{x}/{y}.png').addTo(map);
+    this.map = L.map( this.map_element ).setView([47.589841, -122.319202], 12);
+    L.tileLayer('http://{s}.tiles.mapbox.com/v3/spatialdev.map-c9z2cyef/{z}/{x}/{y}.png').addTo( this.map );
 
     // add the sidebar
     this.add_sidebar();
@@ -190,8 +187,10 @@ MapView.prototype.add_layer = function( layer, name, title, zIndex, on ) {
 
 MapView.prototype.format_bounds = function( bounds, proj ) {
 
-    var gdal = $("input[name='gdal-checkbox']").prop('checked');
-    var lngLat = $("input[name='coord-order']").prop('checked');
+    //var gdal = $("input[name='gdal-checkbox']").prop('checked');
+    //var lngLat = $("input[name='coord-order']").prop('checked');
+    var gdal = true;
+    var lngLat = true;
 
     var formattedBounds = '';
     var southwest = bounds.getSouthWest();
@@ -207,13 +206,13 @@ MapView.prototype.format_bounds = function( bounds, proj ) {
         ymax = northeast.lat.toFixed(6);
     } else {
         var proj_to_use = null;
-        if (typeof(projdefs[proj]) !== 'undefined') {
+        if (typeof( this.projdefs[proj] ) !== 'undefined') {
             // we have it already, then grab it and use it...
-            proj_to_use = projdefs[proj];
+            proj_to_use = this.projdefs[proj];
         } else {
             // We have not used this one yet... make it and store it...
-            projdefs[proj] = new L.Proj.CRS(proj, proj4defs[proj][1]);
-            proj_to_use = projdefs[proj];
+            this.projdefs[proj] = new L.Proj.CRS(proj, this.proj4defs[proj][1]);
+            proj_to_use = this.projdefs[proj];
         }
         southwest = proj_to_use.project(southwest)
         northeast = proj_to_use.project(northeast)
@@ -245,15 +244,13 @@ MapView.prototype.format_tile = function( point, zoom ) {
     return xTile.toString() + ',' + yTile.toString();
 };
 
-MapView.prototype.render = function() {
-    this.$info_box.append( this.info_box_template( {} ) );
-};
-
-
 MapView.prototype.format_point = function( point, proj ) {
 
-    var gdal = $("input[name='gdal-checkbox']").prop('checked');
-    var lngLat = $("input[name='coord-order']").prop('checked');
+    //var gdal = $("input[name='gdal-checkbox']").prop('checked');
+    //var lngLat = $("input[name='coord-order']").prop('checked');
+    var gdal = true;
+    var lngLat = true;
+    
 
     var formattedPoint = '';
     if (proj == '4326') {
@@ -261,13 +258,13 @@ MapView.prototype.format_point = function( point, proj ) {
         y = point.lat.toFixed(6);
     } else {
         var proj_to_use = null;
-        if (typeof(projdefs[proj]) !== 'undefined') {
+        if (typeof( this.projdefs[proj] ) !== 'undefined') {
             // we have it already, then grab it and use it...
-            proj_to_use = projdefs[proj];
+            proj_to_use = this.projdefs[proj];
         } else {
             // We have not used this one yet... make it and store it...
-            projdefs[proj] = new L.Proj.CRS(proj, proj4defs[proj][1]);
-            proj_to_use = projdefs[proj];
+            this.projdefs[proj] = new L.Proj.CRS(proj, this.proj4defs[proj][1]);
+            proj_to_use = this.projdefs[proj];
         }
         point = proj_to_use.project(point)
         x = point.x.toFixed(4);
