2662e9885c19d486964844e5cd02078487ef99e3
sudobangbang <gregcorradini@gmail.com>
2014-11-08 18:16:18 -0800

    Update main.js

--- examples/main.js
+++ examples/main.js
@@ -1,16 +1,11 @@
 var MapView = require( './map.js' );
-
 (function( def ) {
-
     if ( typeof window !== 'undefined' ) {
         window.App = window.App || {};
         window.App = def();
     }
-
 })( function() {
-
     return {
         MapView : MapView ,
     }
-
 });
